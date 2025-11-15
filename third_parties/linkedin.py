9355a30d0163b435c73b79d3625cba6fa6dde368
frank <franklee809@gmail.com>
2025-11-15 21:53:13 +0800

    feat: setup linkedin scrapper

---
+++ third_parties/linkedin.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linked_profile(linkedin_profile_url: str, mock: bool = False):
+    """Scrape information from linkedin profiles"""
+    pass
